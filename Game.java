aa77a8dec7022447d5bb7a6e0e503eb5a4cb870b
EmmaNiu9403 <Emma9403@126.com>
2016-06-28 00:25:29 -0500

    last changes
    last changes

---
+++ Game.java
@@ -0,0 +1,15 @@
+/**
+ * game class
+ */
+package assignment2;
+
+import java.util.Arrays;
+
+public class Game {
+	public static boolean testMode;
+	public Game(boolean val){
+		
+		testMode = val;
+	}
+
+}
---
+++ GameBoard.java
@@ -0,0 +1,20 @@
+/**
+ * gameboard class
+ */
+package assignment2;
+
+import java.util.Random;
+
+public class GameBoard {
+
+	public String []  GeneratePattern(String [] colors, int numberOfPegs, int colorsNumber){
+		String [] pattern = new String[numberOfPegs];
+		for(int j=0;j<numberOfPegs;j++){
+			Random num = new Random();
+			int i = num.nextInt(colorsNumber);
+			pattern[j]=colors[i];
+		}
+		return pattern;
+		
+	}
+}
